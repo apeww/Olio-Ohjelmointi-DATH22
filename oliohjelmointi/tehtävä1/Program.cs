5948e74ec8c731bf749776b8d04244360a04f725
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-09 12:45:17 +0200

    harjotus1

---
+++ oliohjelmointi/tehtävä1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ajoneuvot ajoneuvot1 = new ajoneuvot("Bugatti", 400, 4);
+
+            ajoneuvot1.TulostaData();
+        }
+    }
+}
---
+++ oliohjelmointi/tehtävä1/ajoneuvot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä1
+{
+    internal class ajoneuvot
+    {
+        public string Nimi;
+        public int Nopeus;
+        public int Renkaat;
+
+        public ajoneuvot(string nimi, int nopeus, int renkaat)
+        {
+            Nimi = nimi;
+            Nopeus = nopeus;
+            Renkaat = renkaat;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Auton nimi on: " + Nimi);
+            Console.WriteLine("Auton Nopeus on. "+ Nopeus);
+            Console.WriteLine("Auton renkaiden määrä on: " + Renkaat);
+        }
+
+
+    }
+}
