054e7d140e3553ebeddf20bdf74e56b18a5c79ec
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-09 13:03:42 +0200

    harjotus2

---
+++ oliohjelmointi/harjotus2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjotus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            opiskelija Opiskelija1 = new opiskelija("Aapo", "DATH22", 100);
+
+            Opiskelija1.TulostaData();
+            Opiskelija1.MuokkaaOpintoPisteitä(5);
+            Opiskelija1.TulostaData();
+
+
+        }
+    }
+}
---
+++ oliohjelmointi/harjotus2/opiskelija.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjotus2
+{
+    internal class opiskelija
+    {
+        public string Nimi;
+        public string OpiskelijaID;
+        public int Opintopisteet;
+
+        public opiskelija(string nimi, string opiskelijaID, int opintopisteet)
+        {
+            Nimi = nimi;
+            OpiskelijaID = opiskelijaID;
+            Opintopisteet = opintopisteet;
+        }
+        public void TulostaData()
+        {
+            Console.WriteLine("opiskelijan nimi on: " + Nimi);
+            Console.WriteLine("Opiskelijan ID on: " + OpiskelijaID);
+            Console.WriteLine("Opiskleijan opinto pisteet on: " + Opintopisteet);
+        }
+        public void MuokkaaOpintoPisteitä(int määrä)
+        {
+            Opintopisteet += määrä;
+        }
+    }
+}
