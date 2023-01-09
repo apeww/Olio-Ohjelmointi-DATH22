fb933dd64127c771f1f65dcbbccb42bfa8c3904c
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-09 13:30:48 +0200

    harjotus3

---
+++ oliohjelmointi/harjotus3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjotus3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kiuas kiuas1 = new kiuas(false, 100, 50);
+
+            kiuas1.TulostData();
+            kiuas1.MuokkaaLämpötilaa(10);
+            kiuas1.MuokkaaKosteutta(10);
+            kiuas1.OnkoKiuasPäällä();
+        }
+    }
+}
---
+++ oliohjelmointi/harjotus3/kiuas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjotus3
+{
+    internal class kiuas
+    {
+        public bool OnkoPäällä = false;
+        public int lämpötila;
+        public int kosteus;
+
+        public kiuas (bool onkoPäällä, int lämpötila, int kosteus)
+        {
+            OnkoPäällä = onkoPäällä;
+            this.lämpötila = lämpötila;
+            this.kosteus = kosteus;
+        }
+        public void TulostData()
+        {
+            Console.WriteLine("Onko kiuas päällä: " + OnkoPäällä);
+            Console.WriteLine("Kiukaan Lämpötila: " + lämpötila);
+            Console.WriteLine("Kosteus: " + kosteus);
+        }
+        public void MuokkaaLämpötilaa(int määrä)
+        {
+            lämpötila += määrä;
+        }
+        public void MuokkaaKosteutta(int määrä)
+        {
+            kosteus += määrä;
+        }
+
+        public void OnkoKiuasPäällä()
+        {
+            if (OnkoPäällä)
+            {
+                Console.WriteLine("kiuas on päällä");
+            }
+            else
+            {
+                Console.WriteLine("Kiuas ei ole päällä");
+            }
+        }
+    }
+}
