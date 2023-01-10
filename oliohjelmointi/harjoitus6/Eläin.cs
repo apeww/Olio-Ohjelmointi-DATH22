9c552eba67eaa81f69cd79e7de22e3e641d9e01a
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-10 11:46:41 +0200

    harjoitus 6

---
+++ oliohjelmointi/harjoitus6/Eläin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus6
+{
+    internal class Eläin
+    {
+        protected string nimi;
+        protected int ika;
+        protected static int eläintenMäärä = 0;
+
+        public Eläin(string nimi, int ika)
+        {
+            this.nimi = nimi;
+            this.ika = ika;
+            eläintenMäärä++;
+        }
+
+        public virtual string Ääni()
+        {
+            return "generic animal sound";
+        }
+
+        public virtual string Tiedot()
+        {
+            return "Eläin: " + nimi + ", ikä: " + ika + " vuotta.";
+        }
+
+        public static int EläintenMäärä()
+        {
+            return eläintenMäärä;
+        }
+    }
+
+    class Koira : Eläin
+    {
+        public Koira(string nimi, int ika) : base(nimi, ika) { }
+
+        public override string Ääni()
+        {
+            return "hau hau";
+        }
+
+        public override string Tiedot()
+        {
+            return "Koira: " + nimi + ", ikä: " + ika + " vuotta.";
+        }
+    }
+
+    class Kissa : Eläin
+    {
+        public Kissa(string nimi, int ika) : base(nimi, ika) { }
+
+        public override string Ääni()
+        {
+            return "miau miau";
+        }
+
+        public override string Tiedot()
+        {
+            return "Kissa: " + nimi + ", ikä: " + ika + " vuotta.";
+        }
+    }
+}
---
+++ oliohjelmointi/harjoitus6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Koira koira = new Koira("Musti", 5);
+            Kissa kissa = new Kissa("Timo", 3);
+
+            Console.WriteLine("Luotujen eläinten määrä: " + Eläin.EläintenMäärä());
+            Console.WriteLine("Ensimmäinen eläin: " + koira.Tiedot() + " Ääni: " + koira.Ääni());
+            Console.WriteLine("Toinen eläin: " + kissa.Tiedot() + " Ääni: " + kissa.Ääni());
+
+            Console.ReadLine();
+        }
+    }
+}
