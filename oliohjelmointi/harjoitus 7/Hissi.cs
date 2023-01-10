06f4d8e7b035ea0563fab000af61d550084c0e88
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-10 12:10:01 +0200

    harjoitus 7

---
+++ oliohjelmointi/harjoitus 7/Hissi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_7
+{
+    internal class Hissi
+    {
+        private int kerros;
+        public int Kerros
+        {
+            get { return kerros; }
+            set
+            {
+                if (value >= 1 && value <= 6)
+                {
+                    kerros = value;
+                    Console.WriteLine("Hissi on nyt kerroksessa " + kerros);
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen kerros, anna kerros 1-6 väliltä.");
+                }
+            }
+        }
+        public void MeneKerrokseen()
+        {
+            while (true)
+            {
+                Console.WriteLine("Mihin kerrokseen haluat mennä? (1-6)");
+                int kerros = int.Parse(Console.ReadLine());
+                Kerros = kerros;
+            }
+        }
+    }
+}
---
+++ oliohjelmointi/harjoitus 7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Hissi hissi = new Hissi();
+                hissi.MeneKerrokseen();
+            
+
+        }
+    }
+}
