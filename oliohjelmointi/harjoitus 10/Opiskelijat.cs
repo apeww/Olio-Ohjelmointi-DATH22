07d3fc41985f781d8e2dc41a725a9f50c4f24659
Aapo Lehokas <aapo.lehokas@student.careeria.fi>
2023-01-13 10:33:48 +0200

    Harjoitus 11

---
+++ oliohjelmointi/harjoitus 10/Opiskelijat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_10
+{
+    internal class Opiskelijat
+    {
+        public string Etunimi;
+        public string Sukunimi;
+        public string Ryhmätunnus;
+        public string Henkilötunnus;
+
+        public Opiskelijat(string etunimi, string sukunimi, string ryhmätunnus, string henkilötunnus)
+        {
+            Etunimi = etunimi;
+            Sukunimi = sukunimi;
+            Ryhmätunnus = ryhmätunnus;
+            Henkilötunnus = henkilötunnus;
+        }
+    }
+}
---
+++ oliohjelmointi/harjoitus 10/Program.cs
@@ -0,0 +1,42 @@
+﻿using harjoitus_10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Opiskelijat> HenkilöSanakirja = new Dictionary<string, Opiskelijat>();
+
+            Opiskelijat Andrew = new Opiskelijat("Andrew", "Tate", "DATH22", "031106-1245");
+            Opiskelijat Aapo = new Opiskelijat("Aapo", "Lehokas", "DATH22", "120346-1345");
+
+
+
+            try
+            {
+                HenkilöSanakirja.Add(Andrew.Henkilötunnus, Andrew);
+                HenkilöSanakirja.Add(Aapo.Henkilötunnus, Aapo);
+                HenkilöSanakirja.Add(Aapo.Henkilötunnus, Aapo);
+
+
+                Console.WriteLine("Henkilöita Sanakirjassa: " + HenkilöSanakirja.Count);
+
+                HenkilöSanakirja.Remove("031106-1245");
+
+                Console.WriteLine("Henkilöitä Sanakirjassa: " + HenkilöSanakirja.Count);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("VIRHE ERROR KOODI: " + e.Message);
+            }
+
+        }
+    }
+}
